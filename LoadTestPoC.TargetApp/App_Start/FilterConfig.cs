00a8b0a10e8c1a20b24d73f6584fc50e6d0fb7a8
Andrea Coluccio <ancolucc@microsoft.com>
2017-08-01 10:47:06 +0100

    Add Telemetry

--- LoadTestPoC.TargetApp/App_Start/FilterConfig.cs
+++ LoadTestPoC.TargetApp/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace LoadTestPoC.TargetApp
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
